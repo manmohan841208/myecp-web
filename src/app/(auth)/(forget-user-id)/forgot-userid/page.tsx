50978a02f0cf7f17bd04ed11725b83024f409455
adalmohit <mohit.pratap@alshaya.com>
2025-10-30 15:53:37 +0530

    fixes

--- src/app/(auth)/(forget-user-id)/forgot-userid/page.tsx
+++ src/app/(auth)/(forget-user-id)/forgot-userid/page.tsx
@@ -201,6 +201,7 @@ export default function RecoverUserIDPage() {
                   error={errors.SSNLast5?.message}
                   name="SSNLast5"
                   type="password"
+                  className="w-full"
                   maxLength={5}
                   iconRight={
                     errors.SSNLast5?.message || fieldError ? NotSecure : ''
