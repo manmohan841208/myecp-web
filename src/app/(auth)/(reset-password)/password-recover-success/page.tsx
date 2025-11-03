1291e1dcce2daf4199f3182ed08ad1c5c4d72c60
adalmohit <mohit.pratap@alshaya.com>
2025-11-03 15:41:13 +0530

    Resolved build issue

--- src/app/(auth)/(reset-password)/password-recover-success/page.tsx
+++ src/app/(auth)/(reset-password)/password-recover-success/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import Card from '@/components/atoms/Card';
 import Button from '@/components/atoms/Button';
@@ -5,20 +7,22 @@ import CustomAlert from '@/components/atoms/AlertMessage';
 
 const PasswordSuccessfullRecoverPage = () => {
   return (
-    <div className="mx-auto max-w-[1152px] p-4 !text-base ">
+    <div className="mx-auto max-w-[1152px] p-4 !text-base">
       <Card
         className="w-full bg-[var(--color-white)] !p-0 md:max-w-[860px]"
         header="MyECP Password Reset"
       >
-        <div className="flex flex-col p-4 !pb-0 gap-4">
+        <div className="flex flex-col gap-4 p-4 !pb-0">
           <CustomAlert
             type="success"
-             className='py-4'
+            className="py-4"
             description="Your password has been reset successfully for MyECP account."
           />
 
-          <div className="flex items-center justify-end gap-2 mb-4 h-10">
-            <Button variant="primary" className='h-full'>Log In</Button>
+          <div className="mb-4 flex h-10 items-center justify-end gap-2">
+            <Button variant="primary" className="h-full">
+              Log In
+            </Button>
           </div>
         </div>
       </Card>
